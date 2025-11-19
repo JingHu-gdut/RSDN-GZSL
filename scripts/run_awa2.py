a11be28236e6808f8f606a96a1368b4952a28658
JingHu-gdut <3122004522@mail2.gdut.edu.cn>
2025-11-19 18:27:04 +0800

    Create run_awa2.py

---
+++ scripts/run_awa2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""""
+GZSL
+"""
+import os
+os.system('''OMP_NUM_THREADS=8 python train_free.py --gammaD 10 \
+--gammaG 10 --gzsl --encoded_noise --manualSeed 9182 --preprocessing --cuda --image_embedding res101 \
+--class_embedding att --nepoch 51 --ngh 4096 --ndh 4096 --lambda1 10 --critic_iter 1 \
+--feed_lr 0.0001 --dec_lr 0.0001 --a1 0.01 --a2 0.01 \
+--nclass_all 50 --dataroot data --dataset AWA2 \
+--batch_size 64 --nz 85 --latent_size 85 --attSize 85 --resSize 768 \
+--lr 0.00001 --classifier_lr 0.001 --nclass_seen 40 --freeze_dec \
+--syn_num 2400 --center_margin 50 --center_weight 0.5 --recons_weight 1.0 --incenter_weight 0.1 --GA_weight 1.0 \
+--SD_weight 2.0 --NI_weight 1.0 --head_number 1
+''')
+
+""""
+CZSL
+"""
+# import os
+# os.system('''OMP_NUM_THREADS=8 python train_free.py --gammaD 10 \
+# --gammaG 10 --gzsl --encoded_noise --manualSeed 9182 --preprocessing --cuda --image_embedding res101 \
+# --class_embedding att --nepoch 401 --ngh 4096 --ndh 4096 --lambda1 10 --critic_iter 1 \
+# --feed_lr 0.0001 --dec_lr 0.0001 --loop 2 --a1 0.01 --a2 0.01 \
+# --nclass_all 50 --dataroot data --dataset AWA2 \
+# --batch_size 256 --nz 85 --latent_size 85 --attSize 85 --resSize 768 \
+# --lr 0.00001 --classifier_lr 0.001 --nclass_seen 40 --freeze_dec \
+# --syn_num 5600 --center_margin 50 --center_weight 1.5 --recons_weight 0.001 --incenter_weight 0.001 --GA_weight 1.0 \
+# --SD_weight 1.0 --NI_weight 1.0 --head_number 5
+# ''')
